a58bd00d65583e8f71326797c5cef797533a6fc5
Bishoy Asaad <32459444+bishogasaad@users.noreply.github.com>
2018-11-22 15:05:02 +0200

    Update Alter_Function.cpp

--- Alter_Function.cpp
+++ Alter_Function.cpp
@@ -2,12 +2,18 @@
 #include <string>
 using namespace std;
 
-string alter (string msg, int bitPosition, char newValue){
-	msg.at(bitPosition) = newValue;
+string alter (string msg, int bitPosition){
+	if (msg[bitPosition]=='0')msg[bitPosition]='1';
+	else msg[bitPosition]='0';
 	return msg;
 }
 
-int main ()
+int main (int argc, char *argv[])
 {
+	int n=atoi(argv[1]);
+	string msg,crc;
+	cin>>msg;cin>>crc;
+	cout<<alter(msg,n-1)<<endl;
+	cout<<crc<<endl;
 	return 0;
 }
